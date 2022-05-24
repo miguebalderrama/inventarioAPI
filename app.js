1e347226c2edd30d2a2de4a6c74bad6651e6e869
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2022-05-24 00:37:28 -0300

    tenemos los read y los creates

--- app.js
+++ app.js
@@ -1,43 +1,67 @@
-const express = require('express');
-const { send } = require('express/lib/response');
-const mysql = require('mysql');
+const express = require("express");
+const { send } = require("express/lib/response");
+const mysql = require("mysql");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req,res)=> {
-    res.send("Hola mi servidor funciona")
-})
 
-app.get("/api/inventario", (req,res)=> {
-    connection.query("SELECT * FROM inventario_labing", (error, filas)=> {
-        if(error){
-            throw error;
-        }else{
-            res.send(filas);
-        }
+/////////////// CREATE DATA //////////////////////////////////////
+
+app.post("/api/inventario", (req, res) => {
+    const data = {id,inventario,equipo,marca,modelo,categoria} = req.body;
+    const sql = "INSERT INTO inventario_labing SET ?";
+    connection.query(sql, data, (error, results)=>{
+    if(error){
+        throw error;
+    }else{
+        res.send(results);
     }
-        )
-})
+});
+});
+////////////// READ DATA ////////////////////////////////////////
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'inventario_labing'
-
-})
-
-connection.connect(err => { 
-    if(err) {
-        throw err;
+app.get("/api/inventario", (req, res) => {
+  connection.query("SELECT * FROM inventario_labing", (error, filas) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(filas);
     }
-    console.log('Conexion correcta');
-})
-
-
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  });
 });
 
+app.get("/api/inventario/:id", (req, res) => {
+  const id = req.params.id;
+  connection.query("SELECT * FROM inventario_labing WHERE id = ?",[id], (error, fila)=>{    
+    if(error){
+      throw error;
+    } else{
+      res.send(fila);
+    }
+  })
+})
+
+
+/////////////// UPDATE DATA //////////////////////////////////////
+
+
+
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "inventario_labing",
+});
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Conexion correcta");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
